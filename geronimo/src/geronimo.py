31e4b3de9887365abddb11b28d0b1df0375e97b2
Matthias Meisser <digi_c@arcor.de>
2012-06-16 19:49:35 +0000

    code cleanup

--- geronimo/src/geronimo.py
+++ geronimo/src/geronimo.py
@@ -6,33 +6,30 @@ Created on 13.10.2011
 
 @author: Matthias Meisser
 
-Geronimo is a modular data warehouse for maintaining all sources of information by the OpenNet Initiative.
+Geronimo is a modular data warehouse for maintaining 
+all sources of information by the OpenNet Initiative.
 
-Dependencies: 
+Dependencies (can be installed via PIP): 
     Feedparser    http://code.google.com/p/feedparser/
     Wikitools     code.google.com/p/python-wikitools/
+    SimpleJSON    http://code.google.com/p/simplejson/
 
 Usage:
-
-Configuration
+1. Create a conf.ini file and adapt data paths within
+2. Invoke python geronimo.py
+3. register geronimo as WSGI CGI for Apache
 
 released under the GPL
 '''
 
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-CONFIG_FILE = os.path.join(BASE_DIR, "config.ini")
-WIKI_CACHE_FILE = os.path.join(BASE_DIR, "wiki_nodes.cache")
-LINK_CACHE_FILE = os.path.join(BASE_DIR, "links.cache")
-
 import logging
 import getopt
 import pickle
 from ConfigParser import SafeConfigParser
 
-import helpers.wikimonitor as wikimonitor
+#import helpers.wikimonitor as wikimonitor
 import helpers.wikicache as wikicache
 import helpers.olsrdstats as olsrdstats
 from helpers.lastseen import lastSeenImporter
@@ -41,79 +38,87 @@ from helpers.olsrdstats import OLSRDimporter
 from helpers.cables import cablesImporter
 from primitives import APstat
 
-global logger
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+CONFIG_FILE = os.path.join(BASE_DIR, "config.ini")
+WIKI_CACHE_FILE = os.path.join(BASE_DIR, "wiki_nodes.cache")
+LINK_CACHE_FILE = os.path.join(BASE_DIR, "links.cache")
 
-def __configToDictonary(config, section):
-    dict={}
-    for o, v in config.items(section):
-        dict[o]=v
-    return dict
+def __configToDictonary(conf, section):
+    '''returns dictionary for the options of the log'''
+    dictionary = {}
+    for opt, val in conf.items(section):
+        dictionary[opt] = val
+    return dictionary
 
 def __getLogger():
+    '''init logger'''
     handler = logging.StreamHandler(sys.stdout)    
     logger = logging.getLogger() 
     logger.addHandler(handler)
     return logger 
 
 def __parseArgs():
+    '''parse #arguments'''
     try:
-        opts, rest = getopt.getopt(sys.argv[1:],"d", ["debug"])
+        opts, rest = getopt.getopt(sys.argv[1:], "d", ["debug"])
     except getopt.GetoptError, err:
         print str(err)
-        print "Usage: "
+        print "Usage: "#TODO Debuglevel über Config einstellen
         sys.exit(2)
-    for o, a in opts:
-        if o in ("-d","--debug"):
-            logger.setLevel(logging.DEBUG)
+    for opt, arg in opts:
+        if opt in ("-d","--debug"):
+            log.setLevel(logging.DEBUG)
         
 def __initConfig():
-    config = SafeConfigParser()
-    logging.log(logging.DEBUG, "loading config")
-    config.read(CONFIG_FILE)
-    return config      
+    '''inits conf'''
+    conf = SafeConfigParser()
+    logging.log(logging.DEBUG, "loading conf")
+    conf.read(CONFIG_FILE)
+    return conf      
 
-def __printStats(aps,links):
-    ap_online=0
+def __printStats(aps, links):
+    '''prints results in a single line'''
+    ap_online = 0
     for ap in aps.values():
         if (ap.state == APstat.ONLINE) :
-            ap_online=ap_online+1
-    print "Finished: %(online)u / %(total)u APs online (%(links)u Links)" % {"online":ap_online,"total":len(aps),"links":len(links)}  
+            ap_online = ap_online + 1
+    print "Finished: %(online)u / %(total)u APs online (%(links)u Links)" % {"online":ap_online, "total":len(aps), "links":len(links)}  
 
-def __saveCache(filename,objects):
-    fcache=file(filename,"w")
-    pickle.dump(objects,fcache)
+def __saveCache(filename, objects):
+    '''serialies object container to file'''
+    fcache = file(filename,"w")
+    pickle.dump(objects, fcache)
     fcache.close()
         
 if __name__ == '__main__':
-    global logger
-    logger=__getLogger()
+    log = __getLogger()
     __parseArgs()
     config = __initConfig()
     #update AP state
-    inits=__configToDictonary(config,"lastseen")
-    ls=lastSeenImporter(inits)
-    aps=ls.importLastSeen()
+    inits = __configToDictonary(config, "lastseen")
+    ls = lastSeenImporter(inits)
+    aps = ls.importLastSeen()
     #crawl wiki
-    inits=__configToDictonary(config,"wiki")
-    wiki=wikicache.wikiImporter(inits)
-    aps=wiki.importAPTable(aps)
+    inits = __configToDictonary(config, "wiki")
+    wiki = wikicache.wikiImporter(inits)
+    aps = wiki.importAPTable(aps)
     #get AP details
-    inits=__configToDictonary(config,"DB")
-    db=DBimporter(inits)
-    aps=db.importDB(aps)
+    inits = __configToDictonary(config, "DB")
+    db = DBimporter(inits)
+    aps =db.importDB(aps)
     #Links
-    inits=__configToDictonary(config,"olsrd")
-    olsrd=OLSRDimporter(inits)
-    links=olsrd.importLinks(aps)
-    cables=cablesImporter(links)
-    links=cables.importCables()
-    links=wiki.importBackbones(links)
+    inits = __configToDictonary(config, "olsrd")
+    olsrd = OLSRDimporter(inits)
+    links = olsrd.importLinks(aps)
+    cables = cablesImporter(links)
+    links = cables.importCables()
+    links = wiki.importBackbones(links)
     #Finish
-    __printStats(aps,links)    
-    __saveCache(WIKI_CACHE_FILE,aps)
-    __saveCache(LINK_CACHE_FILE,links)
+    __printStats(aps, links)    
+    __saveCache(WIKI_CACHE_FILE, aps)
+    __saveCache(LINK_CACHE_FILE, links)
     '''
-    inits=__configToDictonary(config,"wiki")
+    inits=__configToDictonary(conf,"wiki")
     wiki=wikicache.wikiImporter(inits)
     wiki.loadAPTemplates()
     
