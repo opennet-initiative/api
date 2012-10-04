29a994b2468e5b2e8aaf014e36354395f42759ec
Matthias Meisser <digi_c@arcor.de>
2012-10-04 16:52:49 +0000

    Formatierung des "OS"-Eintrags verbessert (Leerzeichen entfernt) Code-Stil verbessert

--- geronimo/src/helpers/expDB.py
+++ geronimo/src/helpers/expDB.py
@@ -15,37 +15,41 @@ import primitives
 
 class DBimporter():
     def __init__(self,config):
-        self.__path=config["path"]
+        self.__path = config["path"]
         self.__connect(self.__path)
         
     def __connect(self,path):
-        global con
-        global cur
         logging.log(logging.DEBUG, "db: connecting")
-        con = sqlite3.connect(path)
-        cur = con.cursor()
+        self._con = sqlite3.connect(path)
+        self.cur = self._con.cursor()
         
-    def importDB(self,aps=None):
+    def importDB(self, aps=None):
         logging.log(logging.DEBUG, "db: requesting")
         try:
-            cur.execute('SELECT mainip,sys_board,sys_os_type,sys_os_name,sys_os_rel,on_wifidog_status,on_vpn_status,on_ugw_status FROM nodes')
-            for row in cur:
-                ip=row[0]
-                if aps is not None:
-                    try:
-                        ap=aps[ip]
-                    except KeyError:
-                        aps[ip]=ap
-                else:
-                    ap=primitives.AccesPoint(ip,None)
-                ap.board=self.__parseBoardID(row[1])
-                ap.os=str(row[2]+"("+row[3]+" "+row[4]+")")
-                ap.wifidog=row[5]
-                ap.vpn=row[6]
-                ap.ugw=row[7]
-                aps[ip]=ap
+            self.cur.execute("""SELECT mainip, sys_board, sys_os_type, sys_os_name,
+                        sys_os_rel, on_wifidog_status, on_vpn_status, on_ugw_status
+                    FROM nodes""")
         except sqlite3.OperationalError:
             logging.log(logging.ERROR, "db: importDB() failed")
+            return aps
+        for (mainip, sys_board, sys_os_type, sys_os_name, sys_os_rel, 
+                on_wifidog_status, on_vpn_status, on_ugw_status) in self.cur.fetchall():
+            ip = mainip
+            if aps and (ip in aps):
+                ap = aps[ip]
+            else:
+                ap = primitives.AccesPoint(ip, None)
+            ap.board = self.__parseBoardID(sys_board)
+            ap.os = sys_os_type.strip()
+            os_details = "%s %s" % (sys_os_name.strip(), sys_os_rel.strip())
+            # empty strings are removed from start/end via "strip"
+            if os_details.strip():
+                ap.os += " (%s)" % os_details.strip()
+            ap.wifidog = on_wifidog_status
+            ap.vpn = on_vpn_status
+            ap.ugw = on_ugw_status
+            if aps:
+                aps[ip] = ap
         return aps
         
     def __parseBoardID(self,strBoardID):
@@ -54,26 +58,17 @@ class DBimporter():
                     "0x0101/42":"Linksys WRT54GS",
                     "0x0708/42":"Linksys WRT54G",
                     "bcm94710dev/asusX":"Asus WL500",
-                    "Atheros AR7161 rev 2 Ubiquiti RouterStation Pro": "Ubiquiti Routerstation",
+                    "Atheros AR7161 rev 2 Ubiquiti RouterStation Pro":"Ubiquiti Routerstation",
                     "Atheros AR7240 (Python)  NanoStation M5": "Ubiquiti NanoStration M5",
                     "Atheros AR7240 (Python)  NanoBridge M5":"Ubiquiti NanoBridge M5",
-                    "Atheros AR9132 rev 2 TP-LINK TL-WR1043ND": "TP-LINK TL-WR1043ND",
                     "Atheros AR7240 rev 2 Ubiquiti Nanostation M":"Ubiquiti Nanostation M",
                     "Atheros AR7241 rev 1 Ubiquiti Bullet M":"Ubiquiti Bullet M",
                     "Atheros AR7241 rev 1 Ubiquiti Nanostation M":"Ubiquiti Nanostation M",
+                    "Atheros AR9132 rev 2 TP-LINK TL-WR1043ND": "TP-LINK TL-WR1043ND",
                     "Gateworks Avila Network Platform":"Avila",
                     "Broadcom BCM5352 chip rev 0":"Linksys WRT54G",
                     "Broadcom BCM4712 chip rev 1":"Linksys WRT54G",
                     "Broadcom BCM4712 chip rev 2":"Linksys WRT54G",
                     "":"WRAP 2E-E"}
-        strBoardID=strBoardID.rstrip()
-        try:
-            normStr=board2name[strBoardID]
-        except KeyError:
-            normStr=""
-            logging.log(logging.ERROR, "db: board2name() failed: "+strBoardID)
-        return normStr
-    
-    def __importLinks(self, links):
-        'SELECT mainip, if_name,  wlan_essid, wlan_mode, wlan_channel, wlan_freq FROM ifaces WHERE wlan_essid IS not "" ORDER BY wlan_essid'
-        return None
+        return board2name[strBoardID.strip()]
+
