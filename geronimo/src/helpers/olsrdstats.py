f73e21a3072298e15c97ba4394d17e4c1a3457c6
Matthias Meisser <digi_c@arcor.de>
2012-12-29 18:45:37 +0000

    colorchange back to previous colors

--- geronimo/src/helpers/olsrdstats.py
+++ geronimo/src/helpers/olsrdstats.py
@@ -86,6 +86,31 @@ class OLSRDimporter():
         return startIP+"-"+endIP
 
     def __getETXColor(self,etx):
+      return self.__getETXOrigColor(etx)
+
+    def __getETXOrigColor(self,etx):
+        '''return the original colors used in the old map'''
+        colors=['#FF0000','#FF0400','#FF0D00','#FF1100','#FF1E00','#FF2200',
+          '#FF2600','#FF2B00','#FF3300','#FF3700','#FF3C00','#FF4000','#FF6200',
+          '#FF7300','#FF7700','#FF7B00','#FF8400','#FF8800','#FF9100','#FF9500',
+          '#FFA600','#FFAE00','#FFB300','#FFB700','#FFCC00','#FFD500','#FFDD00',
+          '#FFE100','#FFEE00','#FFF200','#FBFF00','#F2FF00','#D9FF00','#C3FF00',
+          '#B2FF00','#AAFF00','#9DFF00','#95FF00','#91FF00','#8CFF00','#80FF00',
+          '#77FF00','#73FF00','#5EFF00','#48FF00','#44FF00','#40FF00','#37FF00',
+          '#22FF00','#08FF00','#04FF00','#00FF04','#00FF1E','#00FF26','#00FF40',
+          '#00FF4D','#00FF59','#00FF5E','#00FF62','#00FF6F','#00FF80','#00FF8C',
+          '#00FF91','#00FFA6','#00FFAE','#00FFB7','#00FFBB','#00FFBF','#00FFD0',
+          '#00FFD5','#00FFD9','#00FFE6','#00FFF2','#00EEFF','#00DDFF','#00D9FF',
+          '#00CCFF','#00C8FF','#00C3FF','#00BBFF','#00B2FF','#00AAFF','#00A6FF',
+          '#00A1FF','#0091FF','#008CFF','#0088FF','#0084FF','#007BFF','#0077FF',
+          '#0073FF','#0066FF','#0055FF','#004CFF','#0048FF','#0040FF','#002BFF',
+          '#001EFF','#0019FF','#0000FF','#0000FF']
+        try:
+          return colors[int(100/etx)]
+        except IndexError:
+          return "#FF0000"
+
+    def __getETXNewColor(self,etx):
         '''ETX 1.0 is optimal (blue), 3.0 is acceptable(yellow), > is unusable'''
         hue=(10-etx)/10 #limit to 10 and inverse direction
         hue=hue*0.666 #to become 1.0=blue
