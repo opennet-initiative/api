2659813a35b695e4d692fa80cb644328a93d3fa9
Markus Valentin <valy@systemausfall.org>
2015-03-27 17:49:17 +0100

    enable spatialite database

--- geronimo/geronimo/settings.py
+++ geronimo/geronimo/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'geronimo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
