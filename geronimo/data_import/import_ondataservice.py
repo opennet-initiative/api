587aa9d555061545a912bea04065667b334ed1d6
Lars Kruse <devel@sumpfralle.de>
2015-03-28 00:03:56 +0100

    noch mehr ondataservice-Import

--- geronimo/data_import/import_ondataservice.py
+++ geronimo/data_import/import_ondataservice.py
@@ -8,13 +8,7 @@ import re
 import time
 import sqlite3
 
-import elements
-
-
-COLUMN_BLACKLISTS = {
-        "nodes": [r"^db_", "mainip$"],
-        "ifaces": [r"^db_", "mainip$"],
-}
+import oni_model.models
 
 
 def _get_table_meta(conn, table):
@@ -35,19 +29,7 @@ def _get_table_meta(conn, table):
     return [col[1] for col in columns.fetchall()]
 
 
-def _parse_nodes_data(mesh, conn, max_age_seconds=300, create_new=True):
-    """ Retrieve all node information from the "nodes" table.
-        @type mesh: elements.Mesh
-        @param mesh: the mesh network to be updated
-        @type conn: sqlite3.Connection
-        @param conn: an open sql connection
-        @type max_age: int
-        @param max_age: maximum age of a table entry in seconds
-        @type create_new: bool
-        @param create_new: create new nodes in the mesh?
-        @rtype: elements.Mesh
-        @return: the mesh with updated nodes
-    """
+def _parse_nodes_data(conn):
     nodes_columns = _get_table_meta(conn, "nodes")
     def get_row_dict(row, columns):
         result = {}
@@ -59,45 +41,109 @@ def _parse_nodes_data(mesh, conn, max_age_seconds=300, create_new=True):
     query = conn.execute("SELECT * FROM nodes WHERE db_update >= %d" % min_epoch)
     for row in query.fetchall():
         data = get_row_dict(row, nodes_columns)
-        main_ip = data["on_olsrd_mainip"]
-        if main_ip and (create_new or mesh.has_node(main_ip)):
-            node = mesh.get_node(main_ip)
-            for key in nodes_columns:
-                if any([re.search(pattern, key) for pattern in COLUMN_BLACKLISTS["nodes"]]):
-                    continue
-                # key is not blacklisted
-                setattr(node, key, data[key])
     # add network interfaces
     ifaces_columns = _get_table_meta(conn, "ifaces")
     query = conn.execute("SELECT * FROM ifaces WHERE db_update >= %d" % min_epoch)
     for row in query.fetchall():
         data = get_row_dict(row, ifaces_columns)
-        main_ip = data["mainip"]
-        if not create_new and not mesh.has_node(main_ip):
-            continue
-        node = mesh.get_node(main_ip)
-        if not hasattr(node, "links"):
-            node.links = []
-        link = {}
-        for key in ifaces_columns:
-            if any([re.search(pattern, key) for pattern in COLUMN_BLACKLISTS["ifaces"]]):
-                continue
-            # key is not blacklisted
-            link[key] = data[key]
-        node.links.append(link)
+        import_network_interface(data)
 
 
-def parse_ondataservice(mesh=None, db_file="/var/run/olsrd/ondataservice.db", max_age_seconds=300, create_new=True):
-    if mesh is None:
-        mesh = elements.get_mesh()
+# "data" ist ein Dictionary mit den Inhalten aus der ondataservice-sqlite-Datenbank
+def import_accesspoint(data)
+    main_ip = data["on_olsrd_mainip"]
+    if not main_ip:
+        return
+    node, created = oni_model.models.AccessPoint.objects.get_or_create(main_ip=main_ip)
+    mapping = {
+        "sys_os_type": "sys_os_type",
+        "sys_os_name": "sys_os_name",
+    }
+    for key_from, key_to in mapping.items():
+        setattr(node, data[key_from])
+    node.save()
+
+
+# "data" ist ein Dictionary mit den Inhalten aus der ondataservice-sqlite-Datenbank
+def import_network_interface(data):
+    main_ip = data["mainip"]
+    node = oni_model.models.AccessPoint.objects.get(main_ip=main_ip)
+    if not node:
+        # wir legen keine Netzwerk-Interfaces fuer APs an, die noch nicht in der Datenbank sind
+        return
+    # Uebertragung von sqlite-Eintraegen in unser Modell
+    mapping = {
+        "if_name": "if_name",
+        "if_type_bridge": "if_is_bridge",
+        "if_type_bridgedif": "if_is_bridged",
+        "if_hwaddress": "if_hwaddress",
+        "ip_label": "ip_label",
+        "ip_address": "ip_address",
+        "ip_broadcast": "ip_broadcast",
+        "on_networks": "opennet_networks",
+        "on_zones": "opennet_firewall_zones",
+        "on_olsr": "olsr_enabled",
+        "dhcp_start": "dhcp_start",
+        "dhcp_limit": "dhcp_limit",
+        "dhcp_leasetime": "dhcp_leasetime",
+        "dhcp_fwd": "dhcp_forward",
+        "ifstat_collisions": "ifstat_collisions",
+        "ifstat_rx_compressed": "ifstat_rx_compressed",
+        "ifstat_rx_errors": "ifstat_rx_errors",
+        "ifstat_rx_length_errors": "ifstat_rx_length_errors",
+        "ifstat_rx_packets": "ifstat_rx_packets",
+        "ifstat_tx_carrier_errors": "ifstat_tx_carrier_errors",
+        "ifstat_tx_errors": "ifstat_tx_errors",
+        "ifstat_tx_packets": "ifstat_tx_packets",
+        "ifstat_multicast": "ifstat_multicast",
+        "ifstat_rx_crc_errors": "ifstat_rx_crc_errors",
+        "ifstat_rx_fifo_errors": "ifstat_rx_fifo_errors",
+        "ifstat_rx_missed_errors": "ifstat_rx_missed_errors",
+        "ifstat_tx_aborted_errors": "ifstat_tx_aborted_errors",
+        "ifstat_tx_compressed": "ifstat_tx_compressed",
+        "ifstat_tx_fifo_errors": "ifstat_tx_fifo_errors",
+        "ifstat_tx_window_errors": "ifstat_tx_window_errors",
+        "ifstat_rx_bytes": "ifstat_rx_bytes",
+        "ifstat_rx_dropped": "ifstat_rx_dropped",
+        "ifstat_rx_frame_errors": "ifstat_rx_frame_errors",
+        "ifstat_rx_over_errors": "ifstat_rx_over_errors",
+        "ifstat_tx_bytes": "ifstat_tx_bytes",
+        "ifstat_tx_dropped": "ifstat_tx_dropped",
+        "ifstat_tx_heart": "ifstat_tx_heart",
+    }
+    # wir verwenden fuer wlan-Interfaces eine separate Klasse und weitere Attribute fuer wifi-Interfaces
+    if data["ssid"]:
+        interface, created = oni_model.models.WifiNetworkInterface.objects.get_or_create(access_point=node, ifname=data["ifname"])
+        mapping.update({
+            "wlan_essid": "wifi_ssid",
+            "wlan_ap": "wifi_bssid",
+            "wlan_type": "wifi_driver",
+            "wlan_hwmode": "wifi_hwmode",
+            "wlan_mode": "wifi_mode",
+            "wlan_channel": "wifi_channel",
+            "wlan_freq": "wifi_freq",
+            "wlan_txpower": "wifi_signal",
+            "wlan_signal": "wifi_signal",
+            "wlan_noise": "wifi_noise",
+            "wlan_bitrate": "wifi_bitrate",
+            "wlan_crypt": "wifi_crypt",
+            "wlan_vaps": "wifi_vaps_enabled",
+        })
+    else:
+        interface, created = oni_model.models.EthernetNetworkInterface.objects.get_or_create(access_point=node, ifname=data["ifname"])
+    for key_from, key_to in mapping.items():
+        setattr(interface, data[key_from])
+    interface.save()
+
+
+def import_ondataservice(db_file="/var/run/olsrd/ondataservice.db", max_age_seconds=300):
     try:
         conn = sqlite3.connect(db_file)
     except sqlite3.OperationalError as err_msg:
         print("Failed to open ondataservice database (%s): %s" % (db_file, err_msg), file=sys.stderr)
     else:
-        _parse_nodes_data(mesh, conn, max_age_seconds=max_age_seconds, create_new=create_new)
+        _parse_nodes_data(conn)
         conn.close()
-    return mesh
 
 
 if __name__ == "__main__":
@@ -106,4 +152,4 @@ if __name__ == "__main__":
     mesh = parse_ondataservice(db_file=sys.argv[1])
     for a in mesh.nodes:
         print(repr(a))
-    
+
--- geronimo/oni_model/models.py
+++ geronimo/oni_model/models.py
@@ -11,26 +11,29 @@ class AccessPoint(models.Model):
     owner = models.TextField()
     device_model = models.TextField()
 
+    sys_os_type = models.TextField()
+    sys_os_name = models.TextField()
+
 
 class EthernetNetworkInterface(models.Model):
     access_point = models.ForeignKey(AccessPoint)
 
     if_name = models.TextField()
-    if_type_bridge = models.TextField()
-    if_type_bridgedif = models.TextField()
+    if_is_bridge = models.BooleanField(default=False)
+    if_is_bridged = models.BooleanField(default=False)
     if_hwaddress = models.TextField()
     ip_label = models.TextField()
     ip_address = models.IPAddressField()
     ip_broadcast = models.IPAddressField()
-    on_networks = models.TextField()
-    on_zones = models.TextField()
-    on_olsr = models.BooleanField()
+    opennet_networks = models.TextField()
+    opennet_firewall_zones = models.TextField()
+    olsr_enabled = models.BooleanField()
 
     # DHCP
-    dhcp_start = models.IntegerField()
-    dhcp_limit = models.IntegerField()
+    dhcp_range_start = models.IntegerField()
+    dhcp_range_limit = models.IntegerField()
     dhcp_leasetime = models.IntegerField()
-    dhcp_fwd = models.BooleanField()
+    dhcp_forward = models.BooleanField()
 
     # statistics
     ifstat_collisions = models.IntegerField()
@@ -65,19 +68,19 @@ _CHOICES_WIFI_CRYPT = {"plain": "Plain", "wep": "WEP", "wpa2_psk": "WPA2-PSK"}.i
 
 
 class WifiNetworkInterface(EthernetNetworkInterface):
-    ssid = models.TextField()
-    bssid = models.TextField()
-    driver = models.TextField(choices=_CHOICES_WIFI_DRIVER)
-    hwmode = models.TextField(choices=_CHOICES_WIFI_HWMODE)
-    mode = models.TextField(choices=_CHOICES_WIFI_MODE)
-    channel = models.PositiveSmallIntegerField()
-    freq = models.DecimalField(max_digits=6, decimal_places=3)
-    txpower = models.PositiveSmallIntegerField()
-    signal = models.SmallIntegerField()
-    noise = models.SmallIntegerField()
-    bitrate = models.DecimalField(max_digits=6, decimal_places=1)
-    crypt = models.TextField(choices=_CHOICES_WIFI_CRYPT)
-    vaps_enabled = models.BooleanField()
+    wifi_ssid = models.TextField()
+    wifi_bssid = models.TextField()
+    wifi_driver = models.TextField(choices=_CHOICES_WIFI_DRIVER)
+    wifi_hwmode = models.TextField(choices=_CHOICES_WIFI_HWMODE)
+    wifi_mode = models.TextField(choices=_CHOICES_WIFI_MODE)
+    wifi_channel = models.PositiveSmallIntegerField()
+    wifi_freq = models.DecimalField(max_digits=6, decimal_places=3)
+    wifi_txpower = models.PositiveSmallIntegerField()
+    wifi_signal = models.SmallIntegerField()
+    wifi_noise = models.SmallIntegerField()
+    wifi_bitrate = models.DecimalField(max_digits=6, decimal_places=1)
+    wifi_iw_crypt = models.TextField(choices=_CHOICES_WIFI_CRYPT)
+    wifi_vaps_enabled = models.BooleanField()
 
     def __unicode__(self):
         return '%s : %s' % (self.main_ip, self.owner)
