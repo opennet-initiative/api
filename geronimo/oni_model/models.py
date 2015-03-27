02dadce07620d22fe205335ac0d5c66103b6860c
Markus Valentin <valy@systemausfall.org>
2015-03-27 17:50:26 +0100

    migrate model field (lat,long) to Point field

--- geronimo/oni_model/models.py
+++ geronimo/oni_model/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.gis.db import models as gismodels
 
 # Create your models here.
 class Node(models.Model):
     ip=models.IPAddressField(primary_key=True)
     post_address=models.TextField()
     antenna=models.TextField()
-    lat=models.FloatField()
-    long=models.FloatField()
+    position=gismodels.PointField(default=None)
+    objects = gismodels.GeoManager()
     owner=models.TextField()
     device=models.TextField()
 
