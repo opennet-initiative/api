ce2c6253e9b9f060e736df25fbec7a721c0bc5b5
Markus Valentin <valy@systemausfall.org>
2015-03-27 23:45:43 +0100

    add EthernetNetworkInterface WifiNetworkInterface to admin interface

--- geronimo/oni_model/admin.py
+++ geronimo/oni_model/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import AccessPoint
+from .models import AccessPoint,EthernetNetworkInterface,WifiNetworkInterface
 
 admin.site.register(AccessPoint)
+admin.site.register(EthernetNetworkInterface)
+admin.site.register(WifiNetworkInterface)
