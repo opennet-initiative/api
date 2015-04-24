380564dc8f24944773b7bac05712d66f2690c3c4
matthias <mame@plattform32.de>
2015-04-24 23:48:08 +0200

    gis restframework ergänzt

--- geronimo/oni_model/serializer.py
+++ geronimo/oni_model/serializer.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
+from rest_framework_gis import serializers 
 from oni_model.models import AccessPoint
 
-class AccessPointSerializer(serializers.ModelSerializer):
+class AccessPointSerializer(serializers.GeoModelSerializer):
     class Meta:
         model = AccessPoint
     
