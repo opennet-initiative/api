b4e8a80d1a896706e5a368e00db1432ddb6e192d
Lars Kruse <devel@sumpfralle.de>
2015-06-28 17:19:49 +0200

    Skript fuer Ausfuehrung von Aktionen im virtualenv

---
+++ geronimo/scripts/venv_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+BASE_DIR=$(cd "$(dirname "$0")/.."; pwd)
+
+VENV="$1"
+ACTION="$2"
+
+set +eu
+
+. "$HOME/.virtualenvs/$VENV/bin/activate"
+python "$BASE_DIR/manage.py" "$ACTION"
