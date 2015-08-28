9f65eb93fc3cb5a4d80ad07673062a6b924055ea
Lars Kruse <devel@sumpfralle.de>
2015-08-28 03:29:36 +0200

    venv-Skript akzeptiert nun weitere Parameter

--- geronimo/scripts/venv_wrapper.sh
+++ geronimo/scripts/venv_wrapper.sh
@@ -6,8 +6,9 @@ BASE_DIR=$(cd "$(dirname "$0")/.."; pwd)
 
 VENV="$1"
 ACTION="$2"
+shift 2
 
 set +eu
 
 . "$HOME/.virtualenvs/$VENV/bin/activate"
-python "$BASE_DIR/manage.py" "$ACTION"
+python "$BASE_DIR/manage.py" "$ACTION" "$@"
