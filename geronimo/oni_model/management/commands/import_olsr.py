1a521182f83bef99b5fe48739d2be44c33e6b77c
Lars Kruse <devel@sumpfralle.de>
2015-12-10 03:51:54 +0100

    optionale Uebergabe der olsr-txtinfo-URL

--- geronimo/oni_model/management/commands/import_olsr.py
+++ geronimo/oni_model/management/commands/import_olsr.py
@@ -7,5 +7,8 @@ class Command(BaseCommand):
     help = "Importieren der AP-Daten via olsrd-txtinfo"
 
     def handle(self, *args, **options):
-        data_import.import_olsr.import_routes_from_olsr()
-
+        if args:
+            olsrd_txtinfo_url = args[0]
+        else:
+            olsrd_txtinfo_url = None
+        data_import.import_olsr.import_routes_from_olsr(txtinfo_url=olsrd_txtinfo_url)
