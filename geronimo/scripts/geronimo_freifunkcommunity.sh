7768da64db2805660cd8ae80115873065a2d898d
Mathias Mahnke <ap27@opennet-initiative.de>
2015-12-28 15:22:23 +0100

    improved generator, started to read cfg input, json export ready

--- geronimo/scripts/geronimo_freifunkcommunity.sh
+++ geronimo/scripts/geronimo_freifunkcommunity.sh
@@ -25,13 +25,35 @@ declare -A COMMUNITY_LIST
 # read variables
 . "$HOME/$CFG"
 
-# process json template
-COMMUNITY_LIST_NAME="Rostock"
-COMMUNITY_LIST_LAT=54
-COMMUNITY_LIST_LON=14
+# default json variables
+COMMUNITY_LIST_KEY="rostock"
+COMMUNITY_LIST_NAME="rostock"
+COMMUNITY_LIST_LAT=54.0914
+COMMUNITY_LIST_LON=12.1151
 DATEISO=$(date "+%FT%T%Z")
+
+# retrieve requested key via input
+if [ $# -gt 0 ]; then
+    COMMUNITY_LIST_KEY="$1"
+fi
+
+# set json variables from cfg
+[ -z "${COMMUNITY_LIST["$COMMUNITY_LIST_KEY"]+exists}" ] && echo -e >&2 "Error - key '$COMMUNITY_LIST_KEY' not found in cfg file" && exit 1
+COMMUNITY_LIST_ARRAY="${COMMUNITY_LIST["$COMMUNITY_LIST_KEY"]}"
+echo "$COMMUNITY_LIST_ARRAY"
+
+# process json template
+OUTPUT="$JSON_TMP/$JSON_NAME$COMMUNITY_LIST_NAME.json"
+TMP_OUTPUT="$OUTPUT.tmp"
+echo > "$TMP_OUTPUT"
 while read LINE; do
-   eval echo "$LINE" 
+   eval echo "$LINE" >> "$TMP_OUTPUT" 
 done < "$HOME/$JSON"
+
+# format output
+#cat "$TMP_OUTPUT" | jq '.'
+
+# clear temporary files
+rm "$TMP_OUTPUT"
 
 exit 0
