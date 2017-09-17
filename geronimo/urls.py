d9095097797f46d0b8090a083afc7cf011f56020
Lars Kruse <devel@sumpfralle.de>
2017-09-17 04:59:16 +0200

    URL-Definition für Django aktualisiert

--- geronimo/urls.py
+++ geronimo/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'gero_api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^api/', include('oni_model.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
--- oni_model/urls.py
+++ oni_model/urls.py
@@ -1,21 +1,20 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
+
 from oni_model import views
 
-v1_urlpatterns = patterns(
-        '',
-        url(r'^accesspoint/$', views.AccessPointList.as_view()),
-        url(r'^accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
-            views.AccessPointDetail.as_view()),
-        url(r'^link/$', views.AccessPointLinksList.as_view()),
-        url(r'^accesspoint/(?P<ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/links/$',
-            views.AccessPointLinksDetail.as_view()),
-        url(r'^interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/accesspoint/$',
-            views.NetworkInterfaceAccessPoint.as_view()),
-        url(r'^interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
-            views.NetworkInterfaceDetail.as_view()),
-        url(r'^interface/$', views.AccessPointInterfacesList.as_view()),
-        url(r'^accesspoint/(?P<ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/interfaces/$',
-            views.AccessPointInterfacesDetail.as_view()),
-        )
 
-urlpatterns = patterns('', url(r'v1/', include(v1_urlpatterns)))
+urlpatterns = [
+    url(r'^v1/accesspoint/$', views.AccessPointList.as_view()),
+    url(r'^v1/accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
+        views.AccessPointDetail.as_view()),
+    url(r'^v1/link/$', views.AccessPointLinksList.as_view()),
+    url(r'^v1/accesspoint/(?P<ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/links/$',
+        views.AccessPointLinksDetail.as_view()),
+    url(r'^v1/interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/accesspoint/$',
+        views.NetworkInterfaceAccessPoint.as_view()),
+    url(r'^v1/interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
+        views.NetworkInterfaceDetail.as_view()),
+    url(r'^v1/interface/$', views.AccessPointInterfacesList.as_view()),
+    url(r'^v1/accesspoint/(?P<ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/interfaces/$',
+        views.AccessPointInterfacesDetail.as_view()),
+]
