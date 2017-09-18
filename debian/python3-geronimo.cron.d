a9f791416c67e236880610809294d5d428e5c810
Lars Kruse <devel@sumpfralle.de>
2017-09-18 06:36:53 +0200

    Debian: cron-Jobs hinzugefügt

---
+++ debian/python3-geronimo.cron.d
@@ -0,0 +1,6 @@
+# Die untenstehenden Imports benoetigen jeweils einen Datenbank-Lock - also nie parallel ausfuehren
+# Wiki: stuendlich
+13 * * * *      su -c "geronimo-manage import_wiki" on-geronimo >/dev/null
+# ondata: die Verfallsdauer der Informationen ist aufgrund eines olsr-ondataservice-Bugs wohl recht kurz (Minuten)
+*/15 * * * *    su -c "geronimo-manage import_olsr http://localhost:2006/" on-geronimo >/dev/null; su -c "geronimo-manage import_ondataservice" on-geronimo >/dev/null
+
