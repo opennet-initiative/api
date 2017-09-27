f348af926effa29991e6be49cb3a1b26ee36bd1d
Lars Kruse <devel@sumpfralle.de>
2017-09-27 04:46:48 +0200

    Django-Konfiguration: typo für rest_framework_gis korrigiert

--- on_geronimo/settings.py
+++ on_geronimo/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'rest_framework',
-    'rest_framework.gis',
+    'rest_framework_gis',
     'on_geronimo.oni_model',
 )
 
