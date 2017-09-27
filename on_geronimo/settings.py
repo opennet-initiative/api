6193899e5c9302500c66630427f9a3e92babf7ec
Lars Kruse <devel@sumpfralle.de>
2017-09-27 02:54:45 +0200

    TEMPLATES vordefiniert (für html-Ausgabe)

--- on_geronimo/settings.py
+++ on_geronimo/settings.py
@@ -49,6 +49,25 @@ REST_FRAMEWORK = {
     )
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 ROOT_URLCONF = 'on_geronimo.urls'
 
 WSGI_APPLICATION = 'on_geronimo.wsgi.application'
