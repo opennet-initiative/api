c2b74e5cfaaccc0017b07d360f685795c354325a
Lars Kruse <devel@sumpfralle.de>
2017-09-28 00:38:45 +0200

    cronjob "import_olsr" korrigiert

--- debian/on-geronimo-api.cron.d
+++ debian/on-geronimo-api.cron.d
@@ -1,6 +1,6 @@
 # Die untenstehenden Imports benoetigen jeweils einen Datenbank-Lock - also nie parallel ausfuehren
 # Wiki: stuendlich
 13 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_wiki >/dev/null
-*/6 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_olsr http://localhost:2006/ >/dev/null
+*/6 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_olsr >/dev/null
 # ondata: die Verfallsdauer der Informationen ist aufgrund eines olsr-ondataservice-Bugs wohl recht kurz (Minuten)
 */15 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_ondataservice >/dev/null
