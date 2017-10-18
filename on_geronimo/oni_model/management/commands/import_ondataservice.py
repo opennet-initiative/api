fcfcf7542ed3429b5efb3962a8455054cbbf4eb3
Lars Kruse <devel@sumpfralle.de>
2017-10-18 19:52:33 +0200

    Parameterverwendung des Management-Kommandos 'import_ondataservice' korrigiert

--- on_geronimo/oni_model/management/commands/import_ondataservice.py
+++ on_geronimo/oni_model/management/commands/import_ondataservice.py
@@ -1,3 +1,5 @@
+import argparse
+
 from django.core.management.base import BaseCommand
 
 import on_geronimo.data_import.import_ondataservice
@@ -8,8 +10,11 @@ class Command(BaseCommand):
     args = "db_file"
     help = "Importieren der AP-Daten aus einer lokalen ondataservice-Datenbank"
 
+    def add_arguments(self, parser):
+        parser.add_argument('database_file', nargs='?', type=argparse.FileType("r"))
+
     def handle(self, *args, **options):
         kwargs = {}
-        if args:
-            kwargs["db_file"] = args[0]
+        if options["database_file"]:
+            kwargs["db_file"] = options["database_file"].name
         on_geronimo.data_import.import_ondataservice.import_from_ondataservice(**kwargs)
