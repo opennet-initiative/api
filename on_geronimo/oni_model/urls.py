fc3cd71f5a464cee82a37b926708cae3cb08a544
Lars Kruse <devel@sumpfralle.de>
2017-10-19 00:39:55 +0200

    URLs übersichtlicher gestalten

--- on_geronimo/oni_model/urls.py
+++ on_geronimo/oni_model/urls.py
@@ -4,18 +4,17 @@ from django.views.decorators.cache import cache_page
 from on_geronimo.oni_model import views
 
 
+ap_path_prefix = r"^v1/accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
+iface_path_prefix = r"^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
+
+
 urlpatterns = [
-    url(r'^v1/accesspoint/$', views.AccessPointList.as_view()),
-    url(r'^v1/accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
-        views.AccessPointDetail.as_view()),
-    url(r'^v1/accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/interfaces/$',
-        views.AccessPointInterfacesDetail.as_view()),
-    url(r'^v1/accesspoint/(?P<pk>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/links/$',
-        views.AccessPointLinksDetail.as_view()),
-    url(r'^v1/interface/$', views.AccessPointInterfacesList.as_view()),
-    url(r'^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))$',
-        views.NetworkInterfaceDetail.as_view()),
-    url(r'^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/accesspoint/$',
-        views.NetworkInterfaceAccessPoint.as_view()),
+    url(r"^v1/accesspoint/$", views.AccessPointList.as_view()),
+    url(ap_path_prefix + "$", views.AccessPointDetail.as_view()),
+    url(ap_path_prefix + "/interfaces/$", views.AccessPointInterfacesDetail.as_view()),
+    url(ap_path_prefix + "/links/$", views.AccessPointLinksDetail.as_view()),
+    url(r"^v1/interface/$", views.AccessPointInterfacesList.as_view()),
+    url(iface_path_prefix + "$", views.NetworkInterfaceDetail.as_view()),
+    url(iface_path_prefix + "/accesspoint/$", views.NetworkInterfaceAccessPoint.as_view()),
     url(r'^v1/link/$', cache_page(150)(views.AccessPointLinksList.as_view())),
 ]
