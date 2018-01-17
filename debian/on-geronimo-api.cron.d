cb7f71979a048ba5ca3412ec05cd9ca15edd96da
Lars Kruse <devel@sumpfralle.de>
2018-01-17 01:46:54 +0100

    cron-Jobs: via chronic ausführen für reduzierten Mailversand

--- debian/on-geronimo-api.cron.d
+++ debian/on-geronimo-api.cron.d
@@ -1,6 +1,6 @@
 # Die untenstehenden Imports benoetigen jeweils einen Datenbank-Lock - also nie parallel ausfuehren
 # Wiki: stuendlich
-13 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_wiki >/dev/null
-*/6 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_olsr >/dev/null
+13 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage import_wiki
+*/6 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage import_olsr
 # ondata: die Verfallsdauer der Informationen ist aufgrund eines olsr-ondataservice-Bugs wohl recht kurz (Minuten)
-*/15 *	* * *	on-geronimo-api	/usr/bin/on-geronimo-manage import_ondataservice >/dev/null
+*/15 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage import_ondataservice
