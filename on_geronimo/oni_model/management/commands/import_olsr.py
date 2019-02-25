0444be2091ce460492659781241249158454a68d
Lars Kruse <devel@sumpfralle.de>
2019-02-25 05:30:45 +0100

    import_olsr: optionally specify txtinfo URL via argument

--- on_geronimo/oni_model/management/commands/import_olsr.py
+++ on_geronimo/oni_model/management/commands/import_olsr.py
@@ -7,8 +7,11 @@ class Command(BaseCommand):
 
     help = "Importieren der AP-Daten via olsrd-txtinfo"
 
+    def add_arguments(self, parser):
+        parser.add_argument("txtinfo_url", nargs="?", help="URL of OLSR httpinfo service")
+
     def handle(self, *args, **options):
         import_args = {}
-        if args:
-            import_args["txtinfo_url"] = args[0]
+        if options["txtinfo_url"]:
+            import_args["txtinfo_url"] = options["txtinfo_url"]
         on_geronimo.data_import.import_olsr.import_routes_from_olsr(**import_args)
