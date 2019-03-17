d25f08e9fe2777132720cce43e9af6e05eea3d3d
Lars Kruse <devel@sumpfralle.de>
2019-03-17 21:02:12 +0100

    Add cronjob for accesspoint site generation to deb package

--- debian/on-geronimo-api.cron.d
+++ debian/on-geronimo-api.cron.d
@@ -4,3 +4,4 @@
 */6 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage import_olsr
 # ondata: die Verfallsdauer der Informationen ist aufgrund eines olsr-ondataservice-Bugs wohl recht kurz (Minuten)
 */15 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage import_ondataservice
+9 *	* * *	on-geronimo-api	/usr/bin/chronic /usr/bin/on-geronimo-manage regenerate_accesspoint_sites
