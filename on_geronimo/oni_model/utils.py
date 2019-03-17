00a37fb65c5dd5dbc2cdaea3b44e1b4cde69b16f
Lars Kruse <devel@sumpfralle.de>
2019-03-17 22:24:10 +0100

    Use non-deprecated SRID access for GIS geometry
    get_srid/set_srid is deprecated

--- on_geronimo/oni_model/utils.py
+++ on_geronimo/oni_model/utils.py
@@ -6,10 +6,10 @@ def get_center_of_points(points):
     if points:
         points = tuple(points)
         # all points must use the same projection
-        assert len(set(point.get_srid() for point in points)) == 1
+        assert len(set(point.srid for point in points)) == 1
         result = Point(sum(point.x for point in points) / len(points),
                        sum(point.y for point in points) / len(points))
-        result.set_srid(points[0].get_srid())
+        result.srid = points[0].srid
         return result
     else:
         return None
