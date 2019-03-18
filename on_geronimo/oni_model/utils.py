961a6580766937ff24bc8f0d3f10c2504c9d7eff
Lars Kruse <devel@sumpfralle.de>
2019-03-18 02:54:48 +0100

    Fix detection of empty input for "get_center_of_points"
    A generator could have been mistaken as a non-empty input before.

--- on_geronimo/oni_model/utils.py
+++ on_geronimo/oni_model/utils.py
@@ -3,10 +3,11 @@ from django.contrib.gis.geos.point import Point
 
 def get_center_of_points(points):
     """ calculate the weighted center of a given set of points """
+    points = tuple(points)
     if points:
-        points = tuple(points)
         # all points must use the same projection
-        assert len(set(point.srid for point in points)) == 1
+        assert len(set(point.srid for point in points)) == 1, \
+                "Positions must share the same coordinate system: {}".format(points)
         result = Point(sum(point.x for point in points) / len(points),
                        sum(point.y for point in points) / len(points))
         result.srid = points[0].srid
