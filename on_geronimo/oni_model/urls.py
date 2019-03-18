d37bcdd8cbcc5cb6eda9429f1c340cc50872919a
Lars Kruse <devel@sumpfralle.de>
2019-03-18 04:17:38 +0100

    Add URL names for reverse resolution

--- on_geronimo/oni_model/urls.py
+++ on_geronimo/oni_model/urls.py
@@ -9,14 +9,17 @@ iface_path_prefix = r"^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d
 
 
 urlpatterns = [
-    url(r"^v1/accesspoint/$", views.AccessPointList.as_view()),
-    url(ap_path_prefix + "$", views.AccessPointDetail.as_view()),
-    url(ap_path_prefix + "/interfaces/$", views.AccessPointInterfacesDetail.as_view()),
-    url(ap_path_prefix + "/links/$", views.AccessPointLinksDetail.as_view()),
-    url(r"^v1/interface/$", views.AccessPointInterfacesList.as_view()),
-    url(iface_path_prefix + "$", views.NetworkInterfaceDetail.as_view()),
-    url(iface_path_prefix + "/accesspoint/$", views.NetworkInterfaceAccessPoint.as_view()),
-    url(r'^v1/link/$', cache_page(150)(views.AccessPointLinksList.as_view())),
-    url(r'^v1/site/$', views.AccessPointSiteList.as_view()),
-    url(r'^v1/site/(?P<pk>\d+)$', views.AccessPointSiteDetail.as_view()),
+    url(r"^v1/accesspoint/$", views.AccessPointList.as_view(), name="accesspoint-list"),
+    url(ap_path_prefix + "$", views.AccessPointDetail.as_view(), name="accesspoint-details"),
+    url(ap_path_prefix + "/interfaces/$", views.AccessPointInterfacesDetail.as_view(),
+        name="accesspoint-interfaces"),
+    url(ap_path_prefix + "/links/$", views.AccessPointLinksDetail.as_view(),
+        name="accesspoint-links"),
+    url(r"^v1/interface/$", views.AccessPointInterfacesList.as_view(), name="interface-list"),
+    url(iface_path_prefix + "$", views.NetworkInterfaceDetail.as_view(), name="interface-details"),
+    url(iface_path_prefix + "/accesspoint/$", views.NetworkInterfaceAccessPoint.as_view(),
+        name="interface-accesspoint"),
+    url(r'^v1/link/$', cache_page(150)(views.AccessPointLinksList.as_view()), name="link-list"),
+    url(r'^v1/site/$', views.AccessPointSiteList.as_view(), name="site-list"),
+    url(r'^v1/site/(?P<pk>\d+)$', views.AccessPointSiteDetail.as_view(), name="site-details"),
 ]
