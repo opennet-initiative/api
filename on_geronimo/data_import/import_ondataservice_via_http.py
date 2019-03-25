a1babc17d247101d35a92fac26edfb5fbcf78153
Lars Kruse <devel@sumpfralle.de>
2019-03-25 04:00:16 +0100

    Remove asynchronous download via aiohttp
    Currently (Debian Stretch) only an older version of aiohttp is
    available.  This older version cannot handle a lot of ssl-related
    details (ignore verification, ...).  Additionally the aiohttp-based
    processing is rather fragile and thus it falls back to synchronous
    processing under most circumstances.
    Maybe we will try it again later ...

--- on_geronimo/data_import/import_ondataservice_via_http.py
+++ on_geronimo/data_import/import_ondataservice_via_http.py
@@ -6,7 +6,6 @@ import ssl
 import sys
 import urllib
 
-import aiohttp
 from django.db import transaction
 
 from on_geronimo.data_import.import_ondataservice import (import_accesspoint,
@@ -78,101 +77,59 @@ def parse_body(body):
     return accesspoint_data, interfaces_data
 
 
-async def download_and_parse_json(http_client, url, use_async):
-    if use_async:
-        for context in (ssl.SSLContext(), ssl.SSLContext(ssl.PROTOCOL_TLSv1)):
-            try:
-                async with http_client.get(
-                        url, ssl=context, allow_redirects=False, chunked=0,
-                        timeout=aiohttp.ClientTimeout(sock_connect=CONNECTION_TIMEOUT,
-                                                      sock_read=CONNECTION_TIMEOUT)) as response:
-                    if response.status != 200:
-                        continue
-                    try:
-                        body = await response.read()
-                    except aiohttp.client_exceptions.ClientPayloadError:
-                        raise TemporaryRetrievalError("Failed to read the full body")
-                    else:
-                        # the data was retrieved successfully
-                        break
-            except ssl.SSLError:
-                pass
-            except aiohttp.client_exceptions.ClientResponseError as exc:
-                if (exc.code == 400) and (exc.message == "unexpected content-length header"):
-                    raise TemporaryRetrievalError(
-                        "Failed to handle response headers cleanly from {}: {}".format(url, exc))
-                else:
-                    raise OndataserviceParseError(
-                        "Failed to download ondataservice data from {}: {}".format(url, exc))
-        raise TemporaryRetrievalError("Failed to connect")
-    else:
-        ssl_no_check_context = ssl.create_default_context()
-        ssl_no_check_context.check_hostname = False
-        ssl_no_check_context.verify_mode = ssl.CERT_NONE
-        try:
-            with urllib.request.urlopen(url, context=ssl_no_check_context,
-                                        timeout=CONNECTION_TIMEOUT) as request:
-                body = request.read()
-        except (ssl.SSLError, OSError):
-            raise TemporaryRetrievalError("Failed to Connect")
+async def download_and_parse_json(url):
+    ssl_no_check_context = ssl.create_default_context()
+    ssl_no_check_context.check_hostname = False
+    ssl_no_check_context.verify_mode = ssl.CERT_NONE
+    try:
+        with urllib.request.urlopen(url, context=ssl_no_check_context,
+                                    timeout=CONNECTION_TIMEOUT) as request:
+            body = request.read()
+    except (ssl.SSLError, OSError):
+        raise TemporaryRetrievalError("Failed to Connect")
     return parse_body(body)
 
 
 async def retrieve_ondataservice_worker(incoming_queue, results, failures, stdout, stderr):
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=1200,
-                                                                   connect=1200)) as http_client:
-        while True:
+    while True:
+        try:
+            accesspoint = incoming_queue.get_nowait()
+        except asyncio.QueueEmpty:
+            break
+        https_url = "https://{}/opennet-ondataservice-export.json".format(accesspoint.main_ip)
+        http_url = "http://{}/opennet-ondataservice-export.json".format(accesspoint.main_ip)
+        for url in (https_url, http_url):
             try:
-                accesspoint = incoming_queue.get_nowait()
-            except asyncio.QueueEmpty:
-                break
-            https_url = "https://{}/opennet-ondataservice-export.json".format(accesspoint.main_ip)
-            http_url = "http://{}/opennet-ondataservice-export.json".format(accesspoint.main_ip)
-            for url, use_async in ((https_url, True),
-                                   (http_url, True),
-                                   (https_url, False),
-                                   (http_url, False)):
-                try:
-                    for _ in range(3):
-                        # retry in case of temporary errors
-                        try:
-                            node_data, interfaces_data = await download_and_parse_json(
-                                http_client, url, use_async)
-                            break
-                        except aiohttp.client_exceptions.ServerDisconnectedError:
-                            pass
-                        except TemporaryRetrievalError:
-                            pass
-                    else:
-                        continue
-                except OndataserviceParseError as exc:
-                    print("Failed to parse ondataservice data for {}: {}"
-                          .format(accesspoint.main_ip, exc))
-                    failures.append("parse")
-                    incoming_queue.task_done()
-                    break
-                except aiohttp.client_exceptions.ClientOSError:
-                    pass
-                except aiohttp.client_exceptions.ClientConnectorError:
-                    # something like "no connection to host"
-                    failures.append("no connect")
-                    incoming_queue.task_done()
-                    break
-                except (ssl.SSLError, aiohttp.client_exceptions.ServerTimeoutError):
-                    # some kind of connection error
-                    pass
+                for _ in range(3):
+                    # retry in case of temporary errors
+                    try:
+                        node_data, interfaces_data = await download_and_parse_json(url)
+                        break
+                    except TemporaryRetrievalError:
+                        pass
                 else:
-                    print("Finished downloading ondataservice dataset via http from: {}"
-                          .format(accesspoint.main_ip), file=stdout)
-                    await results.put(OndataserviceResult(accesspoint, node_data, interfaces_data))
-                    incoming_queue.task_done()
-                    # the download finished successfully - go for the next item
-                    break
-            else:
-                print("Failed to download ondataservice dataset via http from {}"
-                      .format(accesspoint.main_ip), file=stderr)
-                failures.append("download")
+                    continue
+            except OndataserviceParseError as exc:
+                print("Failed to parse ondataservice data for {}: {}"
+                      .format(accesspoint.main_ip, exc))
+                failures.append("parse")
                 incoming_queue.task_done()
+                break
+            except ssl.SSLError:
+                # some kind of connection error
+                pass
+            else:
+                print("Finished downloading ondataservice dataset via http from: {}"
+                      .format(accesspoint.main_ip), file=stdout)
+                await results.put(OndataserviceResult(accesspoint, node_data, interfaces_data))
+                incoming_queue.task_done()
+                # the download finished successfully - go for the next item
+                break
+        else:
+            print("Failed to download ondataservice dataset via http from {}"
+                  .format(accesspoint.main_ip), file=stderr)
+            failures.append("download")
+            incoming_queue.task_done()
 
 
 @transaction.atomic
