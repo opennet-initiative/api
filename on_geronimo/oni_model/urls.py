opennet-initiative/api

commit b112fcffcd5becf42ce2cfd80be5aedf72bbb87f

Fix interface accesses

on_geronimo/oni_model/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 ap_path_prefix = r"^v1/accesspoint/(?P<main_ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(?P<main_ipv6>[0-9a-fA-F:]+))"
-iface_path_prefix = r"^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
+iface_path_prefix = r"^v1/interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
 
 
 urlpatterns = [

on_geronimo/oni_model/views.py
@@ -288,7 +288,7 @@ class AccessPointInterfacesList(generics.ListAPIView):
 class NetworkInterfaceDetail(DetailView):
     """Ein Interface mit einer spezifischen IP"""
 
-    lookup_field = "addresses__address"
+    lookup_field = "ip_address"
     queryset = EthernetNetworkInterface.objects.all()
     serializer_class = EthernetNetworkInterfaceSerializer
 
@@ -299,7 +299,7 @@ class NetworkInterfaceAccessPoint(DetailView):
     serializer_class = AccessPointSerializer
 
     def retrieve(self, request, ip_address=None):
-        interface = get_object_or_404(EthernetNetworkInterface, addresses_address=ip_address)
+        interface = get_object_or_404(EthernetNetworkInterface, addresses__address=ip_address)
         return Response(self.serializer_class(interface.accesspoint).data)
 
 

commit 814b5e94a3a33fe17adcb2ab1b5caf6f8642fc60

Fix lookup of "interface" addresses
Commit b112fcffcd5 introduced a wrong reference to the address field of
an interface. Thus "/api/v1/interface/SOME_IP/" returned status 500.

on_geronimo/oni_model/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 ap_path_prefix = r"^v1/accesspoint/((?P<main_ip>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))|(?P<main_ipv6>[0-9a-fA-F:]+))"
-iface_path_prefix = r"^v1/interface/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
+iface_path_prefix = r"^v1/interface/(?P<addresses__address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))"
 
 
 urlpatterns = [

on_geronimo/oni_model/views.py
@@ -288,7 +288,7 @@ class AccessPointInterfacesList(generics.ListAPIView):
 class NetworkInterfaceDetail(DetailView):
     """Ein Interface mit einer spezifischen IP"""
 
-    lookup_field = "ip_address"
+    lookup_field = "addresses__address"
     queryset = EthernetNetworkInterface.objects.all()
     serializer_class = EthernetNetworkInterfaceSerializer
 
@@ -298,8 +298,8 @@ class NetworkInterfaceAccessPoint(DetailView):
 
     serializer_class = AccessPointSerializer
 
-    def retrieve(self, request, ip_address=None):
-        interface = get_object_or_404(EthernetNetworkInterface, addresses__address=ip_address)
+    def retrieve(self, request, **kwargs):
+        interface = get_object_or_404(EthernetNetworkInterface, **kwargs)
         return Response(self.serializer_class(interface.accesspoint).data)
 
 