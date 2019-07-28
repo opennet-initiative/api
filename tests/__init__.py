e322edca5a197d458525938e7369d2d04ec21b1a
Lars Kruse <devel@sumpfralle.de>
2019-07-28 05:45:44 +0200

    Test more OLSR import details

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,9 @@
+import logging
 import os
+
+from django.test import TestCase
+
+from on_geronimo.data_import.import_olsr import import_routes_from_olsr_data
 
 
 TEST_ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
@@ -8,3 +13,35 @@ IMPORT_ONDATASERVICE_SAMPLE_FILE = os.getenv("IMPORT_OLSR_SAMPLE_FILE",
                                              os.path.join(TEST_ASSETS_PATH, "ondataservice.db"))
 IMPORT_WIKI_SAMPLE_FILE = os.getenv("IMPORT_WIKI_SAMPLE_FILE",
                                     os.path.join(TEST_ASSETS_PATH, "wiki_nodes.html"))
+
+
+# minimize logging output for all tests by default
+logging.getLogger().setLevel(logging.ERROR)
+
+
+def _read_file_content(filename):
+    with open(filename, encoding="UTF-8") as f:
+        return f.read()
+
+
+def empty_tables():
+    # empty all tables
+    for model in (models.AccessPoint, models.EthernetNetworkInterface,
+                  models.NetworkInterfaceAddress, models.WifiNetworkInterfaceAttributes,
+                  models.RoutingLink, models.InterfaceRoutingLink, models.AccessPointSite):
+        model.objects.all().delete()
+
+
+class TestBase(TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        empty_tables()
+        super().tearDownClass()
+
+    @staticmethod
+    def import_from_olsr(clear_before=False):
+        if clear_before:
+            empty_tables()
+        data = _read_file_content(IMPORT_OLSR_SAMPLE_FILE)
+        import_routes_from_olsr_data(data)
--- tests/test_import_olsr.py
+++ tests/test_import_olsr.py
@@ -1,23 +1,31 @@
-import unittest
+import on_geronimo.oni_model.models as models
 
-import on_geronimo.data_import.import_olsr as olsr
-
-from . import IMPORT_OLSR_SAMPLE_FILE
+from . import IMPORT_OLSR_SAMPLE_FILE, TestBase
 
 
-class OLSRTestcase(unittest.TestCase):
+class OLSRImportTest(TestBase):
 
-    def test_parser_dump(self):
-        with open(IMPORT_OLSR_SAMPLE_FILE, encoding="ascii") as f:
-            lines = f.read().splitlines()
-        tables = olsr._txtinfo_parser(lines, ("routes", "hna", "topology", "mid", "links"))
-        self.assertEqual(len(tables), 5)
-        self.assertGreater(len(tables["routes"]), 0)
-        self.assertGreater(len(tables["hna"]), 0)
-        self.assertGreater(len(tables["topology"]), 0)
-        self.assertGreater(len(tables["mid"]), 0)
-        self.assertGreater(len(tables["links"]), 0)
+    @classmethod
+    def setUpClass(cls):
+        cls.import_from_olsr(clear_before=True)
+        super().setUpClass()
 
+    def test_parsed_counts(self):
+        self.assertEqual(models.AccessPoint.objects.all().count(), 280)
+        self.assertEqual(models.EthernetNetworkInterface.objects.all().count(), 473)
+        self.assertEqual(models.NetworkInterfaceAddress.objects.all().count(), 473)
+        self.assertEqual(models.WifiNetworkInterfaceAttributes.objects.all().count(), 0)
+        self.assertEqual(models.RoutingLink.objects.all().count(), 625)
+        self.assertEqual(models.InterfaceRoutingLink.objects.all().count(), 1250)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_address_assignments(self):
+        for ap, expected_addresses in (
+                ("192.168.1.12", {"192.168.1.12", "192.168.11.12"}),
+                ("192.168.1.54", {"192.168.1.54"}),
+                ("192.168.2.36", {"192.168.2.36", "192.168.12.36"})):
+            with self.subTest(ap=ap):
+                parsed_addresses = set()
+                for interface in models.AccessPoint.objects.get(pk=ap).interfaces.all():
+                    parsed_addresses.update(address.address
+                                            for address in interface.addresses.all())
+                self.assertEqual(expected_addresses, parsed_addresses)
